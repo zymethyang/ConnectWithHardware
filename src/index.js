292536b4ccb039c4bbc984454cf9092fc93c3e8f
Nguyen Minh Tri <zymethyang@gmail.com>
2017-12-30 22:26:50 +0700

    edit for firebase

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors({credentials: true, origin: '*'}));
 
 const port = 80;
-const host = '184.73.182.38';
+const host = 'localhost';
 
 const router = require('./routes');
 const userRouter = require('./userRouter');
